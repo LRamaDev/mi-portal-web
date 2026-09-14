662a2b15b92140ece44e2a11210eb9a1c08aa0aa
LeaRamaDev <leandro.ramallo@gmail.com>
2026-09-14 20:07:04 -0300

    Ingreso V3: renovar cache PWA

--- ingreso-belgrano-monserrat/sw.js
+++ ingreso-belgrano-monserrat/sw.js
@@ -1,4 +1,4 @@
-const CACHE='ingreso-bm-v2';
+const CACHE='ingreso-bm-v3';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./config.js','./manifest.webmanifest','./assets/icon.svg','./data/habilidades.json','./data/ejercicios.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
