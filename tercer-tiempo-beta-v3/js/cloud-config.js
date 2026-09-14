bef9fe5a9f27a34387a9dec29068716a87929920
LeaRamaDev <leandro.ramallo@gmail.com>
2026-09-13 21:34:36 -0300

    Configurar Supabase con publishable key

--- tercer-tiempo-beta-v3/js/cloud-config.js
+++ tercer-tiempo-beta-v3/js/cloud-config.js
@@ -1,8 +1,8 @@
 (function exposeCloudConfig(root) {
   root.TercerTiempoCloudConfig = Object.freeze({
-    enabled: false,
-    supabaseUrl: '',
-    supabaseAnonKey: '',
+    enabled: true,
+    supabaseUrl: 'https://vyacdprekslhrmrlpcgx.supabase.co',
+    supabasePublishableKey: 'sb_publishable_j_UMMBMDRhKaMY2Iji152Q_kkZ48sUG',
     tableName: 'user_app_state'
   });
 })(typeof globalThis !== 'undefined' ? globalThis : window);
