3bb627d219b2c6d1b958b4da6a815da9343841d3
LeaRamaDev <leandro.ramallo@gmail.com>
2026-09-16 12:04:13 -0300

    chore(tercer-tiempo): version Beta v3.4.0

--- tercer-tiempo-beta-v3/js/config.js
+++ tercer-tiempo-beta-v3/js/config.js
@@ -1,7 +1,7 @@
 (function exposeConfig(root) {
   root.TercerTiempoConfig = Object.freeze({
-    appVersion: '3.3.0',
-    versionLabel: 'Beta v3.3.0',
+    appVersion: '3.4.0',
+    versionLabel: 'Beta v3.4.0',
     schemaVersion: 6,
     storageKey: 'tt_app_v1',
     features: Object.freeze({
