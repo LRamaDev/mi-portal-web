38b61b7641835b3d6cb51ce16f3a057dc47990cc
LeaRamaDev <leandro.ramallo@gmail.com>
2026-09-19 14:37:04 -0300

    Tercer Tiempo V4.1.1: actualizar versión

--- tercer-tiempo-v4-beta-ui/config.js
+++ tercer-tiempo-v4-beta-ui/config.js
@@ -1,7 +1,7 @@
 (function exposeConfig(root) {
   root.TercerTiempoConfig = Object.freeze({
-    appVersion: '4.1.0-beta-ui',
-    versionLabel: 'V4.1 Beta UI',
+    appVersion: '4.1.1-beta-ui',
+    versionLabel: 'V4.1.1 Beta UI',
     schemaVersion: 6,
     storageKey: 'tt_app_v1',
     features: Object.freeze({
